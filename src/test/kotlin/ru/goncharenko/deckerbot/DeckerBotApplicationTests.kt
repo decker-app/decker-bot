764f692b1725dd2c020d24b2bd85074e59b9799d
Goncharenko Nikita <goncharenkonikitae@gmail.com>
2023-08-15 17:59:17 +0300

    init commit

---
+++ src/test/kotlin/ru/goncharenko/deckerbot/DeckerBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.goncharenko.deckerbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeckerBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
